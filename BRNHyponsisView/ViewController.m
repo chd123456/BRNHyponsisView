ffd4c95725b37b8353c256187572848a1447868b
崔海达 <949993420@qq.com>
2015-07-30 16:26:07 +0800

    Initial Commit

---
+++ BRNHyponsisView/ViewController.m
@@ -0,0 +1,27 @@
+//
+//  ViewController.m
+//  BRNHyponsisView
+//
+//  Created by 崔海达 on 15/7/30.
+//  Copyright © 2015年 hida. All rights reserved.
+//
+
+#import "ViewController.h"
+
+@interface ViewController ()
+
+@end
+
+@implementation ViewController
+
+- (void)viewDidLoad {
+    [super viewDidLoad];
+    // Do any additional setup after loading the view, typically from a nib.
+}
+
+- (void)didReceiveMemoryWarning {
+    [super didReceiveMemoryWarning];
+    // Dispose of any resources that can be recreated.
+}
+
+@end
